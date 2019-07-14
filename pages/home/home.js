47df1156ac337ea5acd347133eff35fe535272d9
lengleibingfeng <cx1939952554@163.com>
2019-07-14 20:43:59 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,33 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"coderwhy",
+    age:18,
+    students:[
+      {id:110,name:'kdg',age:18},
+      {id:111,name:'gsgs',age:19},
+      {id:112,name:'assg',age:22},
+      {id:113,name:'hjh',age:21}
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    // 1.错误做法：界面不会刷新
+    // this.data.counter+=1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()函数
+    this.setData({
+      counter: this.data.counter +1
+    })
   },
+  handleSubtraction() {
+    // 1.错误做法：界面不会刷新
+    // this.data.counter+=1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 2.this.setData()函数
+    this.setData({
+      counter: this.data.counter -1
+    })
   }
 })
